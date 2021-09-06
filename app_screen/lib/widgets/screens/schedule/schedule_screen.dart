2182c01fd22a59abbec47c3d62fb3a7c328df299
KatoShoma <shoma-1017@i.softbank.jp>
2021-09-06 23:54:10 +0900

    fix:不要なファイルを削除

--- app_screen/lib/widgets/screens/schedule/schedule_screen.dart
+++ app_screen/lib/widgets/screens/schedule/schedule_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:schedule_application/gen/assets.gen.dart';
 import 'package:schedule_application/utils/color/app_color.dart';
 
 class ScheduleScreen extends StatelessWidget {
