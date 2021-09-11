87557b3402a1585364d067dc31aaecefeb709b83
KatoShoma <shoma-1017@i.softbank.jp>
2021-09-12 01:31:36 +0900

    fix:画面遷移を追加

--- app_screen/lib/widgets/screens/schedule/schedule_screen_state.dart
+++ app_screen/lib/widgets/screens/schedule/schedule_screen_state.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:schedule_application/widgets/screens/schedule_setting/schedule_setting_screen.dart';
 import 'package:state_notifier/state_notifier.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -7,4 +9,23 @@ part 'schedule_screen_state.freezed.dart';
 @freezed
 class ScheduleScreenState with _$ScheduleScreenState {
   const factory ScheduleScreenState() = _ScheduleScreenState;
+}
+
+class ScheduleScreenController extends StateNotifier<ScheduleScreenState> with LocatorMixin {
+  final BuildContext context;
+
+  ScheduleScreenController({
+    required this.context,
+  }) : super(const ScheduleScreenState());
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void onTapCreateSchedule() {
+    Navigator.of(context).push(CupertinoPageRoute(
+      builder: (_) => ScheduleSettingScreen(),
+    ));
+  }
 }
