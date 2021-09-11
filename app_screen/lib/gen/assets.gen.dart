bf8b02246b1203693a9532f5b19c9e095b4722c4
KatoShoma <shoma-1017@i.softbank.jp>
2021-09-12 00:16:23 +0900

    fix:Assetgenを正規の形に変更

--- app_screen/lib/gen/assets.gen.dart
+++ app_screen/lib/gen/assets.gen.dart
@@ -8,9 +8,18 @@ import 'package:flutter/widgets.dart';
 class $AssetsImagesGen {
   const $AssetsImagesGen();
 
+  AssetGenImage get aug31 => const AssetGenImage('assets/images/aug_31.png');
   AssetGenImage get back => const AssetGenImage('assets/images/back.png');
-  AssetGenImage get naviSchedule => const AssetGenImage('assets/images/navi_schedule.png');
-  AssetGenImage get naviScheduleOn => const AssetGenImage('assets/images/navi_schedule_on.png');
+  AssetGenImage get bomb => const AssetGenImage('assets/images/bomb.png');
+  AssetGenImage get clock => const AssetGenImage('assets/images/clock.png');
+  AssetGenImage get ghost => const AssetGenImage('assets/images/ghost.png');
+  AssetGenImage get good => const AssetGenImage('assets/images/good.png');
+  AssetGenImage get heart => const AssetGenImage('assets/images/heart.png');
+  AssetGenImage get huHu => const AssetGenImage('assets/images/hu-hu.png');
+  AssetGenImage get naviSchedule =>
+      const AssetGenImage('assets/images/navi_schedule.png');
+  AssetGenImage get naviScheduleOn =>
+      const AssetGenImage('assets/images/navi_schedule_on.png');
   AssetGenImage get naviSettings =>
       const AssetGenImage('assets/images/navi_settings.png');
   AssetGenImage get naviSettingsOn =>
@@ -19,8 +28,13 @@ class $AssetsImagesGen {
       const AssetGenImage('assets/images/navi_share.png');
   AssetGenImage get naviShareOn =>
       const AssetGenImage('assets/images/navi_share_on.png');
-  AssetGenImage get clockIcon =>
-      const AssetGenImage('assets/images/clock.png');
+  AssetGenImage get next => const AssetGenImage('assets/images/next.png');
+  AssetGenImage get person => const AssetGenImage('assets/images/person.png');
+  AssetGenImage get send => const AssetGenImage('assets/images/send.png');
+  AssetGenImage get sendOn => const AssetGenImage('assets/images/send_on.png');
+  AssetGenImage get smail => const AssetGenImage('assets/images/smail.png');
+  AssetGenImage get synergy => const AssetGenImage('assets/images/synergy.png');
+  AssetGenImage get tear => const AssetGenImage('assets/images/tear.png');
 }
 
 class Assets {
