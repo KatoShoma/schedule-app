73eb8bcdfbdedb7d12a45e803dffa1bc1ac3342f
x17062xxtanaka <kait08147110@gmail.com>
2021-09-20 13:21:33 +0900

    fix:設定/childrenの追加

---
+++ app_screen/lib/widgets/screens/setting/children/setting_item_app_version.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:schedule_application/gen/assets.gen.dart';
+import 'package:schedule_application/utils/color/app_color.dart';
+
+class SettingItemAppVersion extends StatelessWidget {
+  final String version;
+
+  const SettingItemAppVersion({
+    Key? key,
+    required this.version,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          border: Border(
+            bottom  : BorderSide(
+              width: 1.0,
+              color: Colors.black12,
+            ),
+          ),
+        ),
+        padding: EdgeInsets.only(
+          top: 13,
+          left: 50,
+          bottom: 8,
+        ),
+        child: Row(
+          children: [
+            Text(
+              'アプリバージョン',
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                color: AppColor.black,
+                fontSize: 20.0,
+              ),
+            ),
+            Spacer(),
+            Text(
+              version,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                color: AppColor.black,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(width: 25.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- app_screen/lib/widgets/screens/setting/setting_screen.dart
+++ app_screen/lib/widgets/screens/setting/setting_screen.dart
@@ -3,14 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:schedule_application/gen/assets.gen.dart';
 import 'package:schedule_application/utils/color/app_color.dart';
+import 'package:schedule_application/widgets/screens/setting/children/setting_item.dart';
+import 'package:schedule_application/widgets/screens/setting/children/setting_item_app_version.dart';
+import 'package:schedule_application/widgets/screens/setting/children/setting_item_onoffswitch.dart';
 
 class SettingScreen extends StatelessWidget {
-  // final bool isOn;
-  // final Function(bool) onChangeIsOn;
   const SettingScreen({
     Key? key,
-    // required this.isOn,
-    // required this.onChangeIsOn,
   }) : super(key: key);
 
   @override
@@ -21,259 +20,33 @@ class SettingScreen extends StatelessWidget {
       ),
       body: Column(
           children: [
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom  : BorderSide(
-                    width: 1.0,
-                    color: Colors.black12,
-                  ),
-                ),
-              ),
-              padding: EdgeInsets.only(
-                top: 13,
-                left: 50,
-                bottom: 8,
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    '通知',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      color: AppColor.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  Spacer(),
-                  CupertinoSwitch(
-                    trackColor: Colors.grey,
-                    value: true,
-                    onChanged:(bool value){},
-                  ),
-                  SizedBox(width: 25.0),
-                ],
-              ),
+
+            SettingOnoffSwitch(
+              title: '通知',
             ),
 
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom  : BorderSide(
-                    width: 1.0,
-                    color: Colors.black12,
-                  ),
-                ),
-              ),
-              padding: EdgeInsets.only(
-                top: 13,
-                left: 50,
-                bottom: 8,
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    'ダークモード',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      color: AppColor.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  Spacer(),
-                  CupertinoSwitch(
-                    trackColor: Colors.grey,
-                    value: true,
-                    onChanged:(bool value){},
-                  ),
-                  SizedBox(width: 25.0),
-                ],
-              ),
+            SettingOnoffSwitch(
+              title: 'ダークモード',
             ),
 
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom  : BorderSide(
-                    width: 1.0,
-                    color: Colors.black12,
-                  ),
-                ),
-              ),
-              padding: EdgeInsets.only(
-                top: 13,
-                left: 50,
-                bottom: 8,
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    'プロフィール設定',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      color: AppColor.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  Spacer(),
-                  ImageIcon(
-                    Assets.images.next,
-                    size: 20,
-                    color: AppColor.gray30,
-                  ),
-                  SizedBox(width: 25.0),
-                ],
-              ),
+            SettingItem(
+              title: 'プロフィール設定',
             ),
 
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom  : BorderSide(
-                    width: 1.0,
-                    color: Colors.black12,
-                  ),
-                ),
-              ),
-              padding: EdgeInsets.only(
-                top: 13,
-                left: 50,
-                bottom: 8,
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    'レビューを書く',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      color: AppColor.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  Spacer(),
-                  ImageIcon(
-                    Assets.images.next,
-                    size: 20,
-                    color: AppColor.gray30,
-                  ),
-                  SizedBox(width: 25.0),
-                ],
-              ),
+            SettingItem(
+              title: 'レビューを書く',
             ),
 
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom  : BorderSide(
-                    width: 1.0,
-                    color: Colors.black12,
-                  ),
-                ),
-              ),
-              padding: EdgeInsets.only(
-                top: 13,
-                left: 50,
-                bottom: 8,
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    '友達に教える',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      color: AppColor.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  Spacer(),
-                  ImageIcon(
-                    Assets.images.next,
-                    size: 20,
-                    color: AppColor.gray30,
-                  ),
-                  SizedBox(width: 25.0),
-                ],
-              ),
+            SettingItem(
+              title: '友達に教える',
             ),
 
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom  : BorderSide(
-                    width: 1.0,
-                    color: Colors.black12,
-                  ),
-                ),
-              ),
-              padding: EdgeInsets.only(
-                top: 13,
-                left: 50,
-                bottom: 8,
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    'ヒントを見る',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      color: AppColor.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  Spacer(),
-                  ImageIcon(
-                    Assets.images.next,
-                    size: 20,
-                    color: AppColor.gray30,
-                  ),
-                  SizedBox(width: 25.0),
-                ],
-              ),
+            SettingItem(
+              title: 'ヒントを見る',
             ),
 
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom  : BorderSide(
-                    width: 1.0,
-                    color: Colors.black12,
-                  ),
-                ),
-              ),
-              padding: EdgeInsets.only(
-                top: 13,
-                left: 50,
-                bottom: 8,
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    'アプリバージョン',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      color: AppColor.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    '1.00',
-                    textAlign: TextAlign.left,
-                    style: const TextStyle(
-                      color: AppColor.black,
-                      fontSize: 20.0,
-                    ),
-                  ),
-                  SizedBox(width: 25.0),
-                ],
-              ),
+            SettingItemAppVersion(
+              version: '1.00',
             ),
             Spacer(),
           ]
