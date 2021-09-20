9653efe988a5911063a69136a14dacf7e6c75d1a
shota nishiyama <shota_4410@icloud.com>
2021-09-20 18:03:30 +0900

    make scheduler algorithm and save dicts into json

--- backend/scheduler/scheduler_algorithm.py
+++ backend/scheduler/scheduler_algorithm.py
@@ -19,4 +19,50 @@ class SchedulerAlgorithm:
         print(f'user_planning_time : {self.user_planning_time}, users_tasks : {self.users_tasks}')
 
     def simple_algorithm(self):
-        pass
+
+        self.give_up_task_dict = {}
+        while True:
+
+            self.total_times = self.sum_task_times(self.users_tasks)
+            self.time_condition = self.user_planning_time - self.total_times
+
+            if self.time_condition >= 0:
+                break
+
+            print('-' * 20)
+            print(f'currently dicts{self.users_tasks}')
+            print('-' * 20)
+
+            self.users_tasks, self.delete_tasks = self.give_up_tasks(self.users_tasks, self.give_up_task_dict)
+
+        return self.users_tasks, self.delete_tasks
+
+    def sum_task_times(self, user_tasks):
+
+        total_times = 0
+        for time in user_tasks.values():
+            total_times += time
+        return total_times
+
+    def give_up_tasks(self, users_tasks, give_up_task_dict):
+
+        long_task = max(users_tasks, key=users_tasks.get)
+
+        print(f'long_task : {long_task}')
+        print(f'users_tasks[long_task] : {users_tasks[long_task]}')
+        # remove long time
+        give_up_task_time = users_tasks[long_task]
+        self.give_up_task_dict[long_task] = give_up_task_time
+
+        del users_tasks[long_task]
+
+        return users_tasks,give_up_task_dict
+
+def scheduler_into_percent(todo_task, user_planning_time):
+
+    scheduler_percent = {}
+
+    for task, time in todo_task.items():
+        scheduler_percent[task] = time / user_planning_time * 100
+
+    return scheduler_percent
