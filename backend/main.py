fd2a92b5c31fd98dec31d81494b295d856d5000e
Ubuntu <narita@schedule-app.g4dksvcrkxyehaiq1sbmuof4ac.xx.internal.cloudapp.net>
2021-09-21 01:46:50 +0000

    websocket対応

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,57 @@
-from flask import Flask
-from scheduler import scheduler
-from user_manager import User_manager
+import json
+import datetime
+import time
+
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+from flask import Flask, request, render_template
+
+from user_manager.User_manager import User_manager
 
 
 app = Flask(__name__)
-
-@app.route('/',methods=["GET"])
-def run_server():
-    global scheduler = scheduler()
-    global user_manager = User_manager()
+app.config.from_object(__name__)
+user_manager = User_manager()
 
 @app.route('/scheduler',methods=["POST"])
 def run_scheduler():
     return hoge
 
-@app.route('/user_manager',methods=["POST"])
-def run_user_manager():
+@app.route('/user_manager_increment',methods=["POST"])
+def user_manager_increment():
     user_manager.increment_counter()
-    return user_manager.get_counter()
 
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port='80')
+@app.route('/pipe')
+def pipe():
+    if request.environ.get('wsgi.websocket'):
+        ws = request.environ['wsgi.websocket']
+        while True:
+            time.sleep(1)
+            message = ws.receive()
+            if message:
+                break 
+            # datetime_now = datetime.datetime.now()
+            # data = {
+            #     'time': str(datetime_now),
+            #     'message': message
+            # }
+            ws.send(get_complate_counter())
+    return
+
+@app.route('/',methods=["GET"])
+def run():
+    return 'hoge'
+
+if __name__ == '__main__':
+    app.debug = True
+
+    host = '0.0.0.0'
+    port = 80
+    host_port = (host, port)
+
+    server = WSGIServer(
+        host_port,
+        app,
+        handler_class=WebSocketHandler
+    )
+    server.serve_forever()
--- backend/user_manager/User_manager.py
+++ backend/user_manager/User_manager.py
@@ -1,9 +1,18 @@
-class User_Manager():
+class User_manager:
+
     def __init__(self):
-        self.counter = 0
+        self.user_counter = 0
+        self.complate_counter = 0
+
     
-    def increment_counter():
-        self.counter = self.counter + 1
+    def increment_user_counter(self):
+        self.user_counter = self.user_counter + 1
+
+    def increment_complate_counter(self):
+        self.complate_counter = self.complate_counter + 1
     
-    def get_counter():
-        return self.counter
+    def get_user_counter(self):
+        return self.user_counter
+    
+    def get_complate_counter(self):
+        return self.complate_counter
